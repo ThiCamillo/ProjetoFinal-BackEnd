9dd2fae6222fc3820ba2ea7035664ed3013baeca
Thiago Camillo <thiago.sam.camillo@gmail.com>
2023-11-05 23:33:20 -0300

    criando classes Vo

---
+++ Projeto Web/senhor-financas/src/main/java/model/vo/UsuarioVo.java
@@ -0,0 +1,88 @@
+package model.vo;
+
+import java.time.LocalDate;
+
+public class UsuarioVo {
+
+	private int idUsuario;
+	private String nome;
+	private String cpf;
+	private String email;
+	LocalDate dataNascimento;
+	private String login;
+	private String senha;
+	
+	public UsuarioVo(int idUsuario, String nome, String cpf, String email, LocalDate dataNascimento, String login,
+			String senha) {
+		super();
+		this.idUsuario = idUsuario;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.dataNascimento = dataNascimento;
+		this.login = login;
+		this.senha = senha;
+	}
+
+	public UsuarioVo() {
+		super();
+	}
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
