0e95e7a1a9a120dd8b9ffb6c134e6d9892a4ec76
Thiago Camillo <thiago.sam.camillo@gmail.com>
2023-11-06 11:28:57 -0300

    criando as classes de model

--- Projeto Web/senhor-financas/src/main/java/model/vo/DespesaVo.java
+++ Projeto Web/senhor-financas/src/main/java/model/vo/DespesaVo.java
@@ -1,5 +1,66 @@
 package model.vo;
 
-public class DespesaVo {
+import java.time.LocalDate;
 
+public class DespesaVo {
+	
+	private int idDespesa;
+	private int idUsuario;
+	private String descricao;
+	private double valor;
+	private LocalDate dataVencimento;
+	private LocalDate dataPagamento;
+	public DespesaVo(int idDespesa, int idUsuario, String descricao, double valor, LocalDate dataVencimento,
+			LocalDate dataPagamento) {
+		super();
+		this.idDespesa = idDespesa;
+		this.idUsuario = idUsuario;
+		this.descricao = descricao;
+		this.valor = valor;
+		this.dataVencimento = dataVencimento;
+		this.dataPagamento = dataPagamento;
+	}
+	public DespesaVo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getIdDespesa() {
+		return idDespesa;
+	}
+	public void setIdDespesa(int idDespesa) {
+		this.idDespesa = idDespesa;
+	}
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public LocalDate getDataVencimento() {
+		return dataVencimento;
+	}
+	public void setDataVencimento(LocalDate dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+	public LocalDate getDataPagamento() {
+		return dataPagamento;
+	}
+	public void setDataPagamento(LocalDate dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+	
+	
+	
 }
--- Projeto Web/senhor-financas/src/main/java/model/vo/ReceitaVo.java
+++ Projeto Web/senhor-financas/src/main/java/model/vo/ReceitaVo.java
@@ -1,5 +1,57 @@
 package model.vo;
 
+import java.time.LocalDate;
+
 public class ReceitaVo {
 
+	private int idReceita;
+	private int idUsuario;
+	private String descricao;
+	private double valor;
+	private LocalDate dataReceita;
+	public ReceitaVo(int idReceita, int idUsuario, String descricao, double valor, LocalDate dataReceita) {
+		super();
+		this.idReceita = idReceita;
+		this.idUsuario = idUsuario;
+		this.descricao = descricao;
+		this.valor = valor;
+		this.dataReceita = dataReceita;
+	}
+	public ReceitaVo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getIdReceita() {
+		return idReceita;
+	}
+	public void setIdReceita(int idReceita) {
+		this.idReceita = idReceita;
+	}
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public LocalDate getDataReceita() {
+		return dataReceita;
+	}
+	public void setDataReceita(LocalDate dataReceita) {
+		this.dataReceita = dataReceita;
+	}
+	
+	
+	
 }
