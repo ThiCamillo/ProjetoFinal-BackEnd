620ab1ea563cb0739f67b83808c23393af9425ae
Thiago Camillo <thiago.sam.camillo@gmail.com>
2023-11-12 01:00:53 -0300

    finished usuarioDao

--- Projeto Web/senhor-financas/src/main/java/model/dao/UsuarioDao.java
+++ Projeto Web/senhor-financas/src/main/java/model/dao/UsuarioDao.java
@@ -76,9 +76,9 @@ public class UsuarioDao {
 			pstmt.setString(6, usuarioVo.getSenha());
 			pstmt.execute();
 			ResultSet resultado = pstmt.getGeneratedKeys();
-			if(resultado.next()) {
+			if (resultado.next()) {
 				usuarioVo.setIdUsuario(resultado.getInt(1));
-		}
+			}
 		} catch (SQLException erro) {
 			System.out.println("\nErro ao executar a query do metodo cadastrarUsuarioDao!");
 			System.out.println("Erro: " + erro.getMessage());
@@ -88,21 +88,18 @@ public class UsuarioDao {
 		}
 		return usuarioVo;
 	}
-	
-	//UPDATE
+
+	// UPDATE
 	public boolean atualizarUsuarioDao(UsuarioVo usuarioVo) {
 		Connection conn = Banco.getConnection();
 		Statement stmt = Banco.getStatement(conn);
 		boolean retorno = false;
-		String query = "UPDATE usuario SET nome = '" + usuarioVo.getNome()
-				+ "', cpf = '" + usuarioVo.getCpf()
-				+ "', email = '" + usuarioVo.getEmail()
-				+ "', datanascimento = '" + usuarioVo.getDataNascimento()
-				+ "', login = '" + usuarioVo.getLogin()
-				+ "', senha = '" + usuarioVo.getSenha()
-				+ "WHERE idusuario = " + usuarioVo.getIdUsuario();
+		String query = "UPDATE usuario SET nome = '" + usuarioVo.getNome() + "', cpf = '" + usuarioVo.getCpf()
+				+ "', email = '" + usuarioVo.getEmail() + "', datanascimento = '" + usuarioVo.getDataNascimento()
+				+ "', login = '" + usuarioVo.getLogin() + "', senha = '" + usuarioVo.getSenha() + "WHERE idusuario = "
+				+ usuarioVo.getIdUsuario();
 		try {
-			if(stmt.executeUpdate(query) == 1) {
+			if (stmt.executeUpdate(query) == 1) {
 				retorno = true;
 			}
 		} catch (SQLException erro) {
@@ -115,14 +112,14 @@ public class UsuarioDao {
 		return retorno;
 	}
 
-	//DELETE
+	// DELETE
 	public boolean excluirUsuarioDao(UsuarioVo usuarioVo) {
 		Connection conn = Banco.getConnection();
 		Statement stmt = Banco.getStatement(conn);
 		boolean retorno = false;
 		String query = "DELETE FROM usuario WHERE idusuario = " + usuarioVo.getIdUsuario();
 		try {
-			if(stmt.executeUpdate(query) == 1) {
+			if (stmt.executeUpdate(query) == 1) {
 				retorno = true;
 			}
 		} catch (SQLException erro) {
@@ -135,7 +132,7 @@ public class UsuarioDao {
 		return retorno;
 	}
 
-	//READ ALL
+	// READ ALL
 	public ArrayList<UsuarioVo> consultarTodosUsuariosDao() {
 		Connection conn = Banco.getConnection();
 		Statement stmt = Banco.getStatement(conn);
@@ -144,7 +141,7 @@ public class UsuarioDao {
 		String query = "SELECT idusuario, nome, cpf, email, datanascimento FROM usuario";
 		try {
 			resultado = stmt.executeQuery(query);
-			while(resultado.next()) {
+			while (resultado.next()) {
 				UsuarioVo usuario = new UsuarioVo();
 				usuario.setIdUsuario(Integer.parseInt(resultado.getString(1)));
 				usuario.setNome(resultado.getString(2));
@@ -164,11 +161,34 @@ public class UsuarioDao {
 		}
 		return listaUsuarios;
 	}
-	
-	//READE ONE
+
+	// READ ONE
 	public UsuarioVo consultarUsuarioDao(UsuarioVo usuarioVo) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = Banco.getConnection();
+		Statement stmt = Banco.getStatement(conn);
+		ResultSet resultado = null;
+		UsuarioVo usuario = new UsuarioVo();
+		String query = "SELECT idusuario, nome, cpf, email, datanascimento FROM usuario" + "WHERE idusuario = "
+				+ usuarioVo.getIdUsuario();
+		try {
+			resultado = stmt.executeQuery(query);
+			if (resultado.next()) {
+				usuario.setIdUsuario(Integer.parseInt(resultado.getString(1)));
+				usuario.setNome(resultado.getString(2));
+				usuario.setCpf(resultado.getString(3));
+				usuario.setEmail(resultado.getString(4));
+				usuario.setDataNascimento(
+						LocalDate.parse(resultado.getString(5), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+			}
+		} catch (SQLException erro) {
+			System.out.println("\nErro ao executar a query do metodo consultarUsuarioDao!");
+			System.out.println("Erro: " + erro.getMessage());
+		} finally {
+			Banco.closeResultSet(resultado);
+			Banco.closeStatement(stmt);
+			Banco.closeConnection(conn);
+		}
+		return usuario;
 	}
 
 }
