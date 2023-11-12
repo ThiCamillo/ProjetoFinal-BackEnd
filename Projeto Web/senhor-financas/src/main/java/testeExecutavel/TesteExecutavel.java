4364d0bb0a18a7158fce40e719547093343d437b
Thiago Camillo <thiago.sam.camillo@gmail.com>
2023-11-12 10:54:25 -0300

    configurando

--- Projeto Web/senhor-financas/src/main/java/testeExecutavel/TesteExecutavel.java
+++ Projeto Web/senhor-financas/src/main/java/testeExecutavel/TesteExecutavel.java
@@ -10,8 +10,13 @@ public class TesteExecutavel {
 
 	
 public static void main(String[] args) {
-	Scanner teclado= new Scanner(System.in);
+	//Scanner teclado= new Scanner(System.in);
 
+	//consultar um usuario
+//	UsuarioVo u1 = new UsuarioVo(1,"","","",null, "", null);
+//	u1 = UsuarioBo.consultarUsuarioBo(u1);
+//	
+	//consultar todos usuarios
 	ArrayList<UsuarioVo> lista = UsuarioBo.consultarTodosUsuariosBo();
 	if(lista.isEmpty()) {
 		System.out.println("tem nada");
