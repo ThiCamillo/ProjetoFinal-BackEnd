568ccfe3872724d58d620c7e4e7da9c6a727e648
Thiago Camillo <thiago.sam.camillo@gmail.com>
2023-11-13 23:11:42 -0300

    finalizado construcao do teste executavel

--- Projeto Web/senhor-financas/src/main/java/testeExecutavel/TesteExecutavel.java
+++ Projeto Web/senhor-financas/src/main/java/testeExecutavel/TesteExecutavel.java
@@ -1,5 +1,6 @@
 package testeExecutavel;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,48 +11,59 @@ public class TesteExecutavel {
 
 public static void main(String[] args) {
 	
-	//UsuarioBo.consultarUsuarioBo(usuarioVo);
-	
-	//Scanner teclado= new Scanner(System.in);
-
 	//USUARIO//
 	UsuarioBo usuarioBo = new UsuarioBo();
 	
-	//cadastrar usuario
-//	UsuarioVo user1 = new UsuarioVo(0,"Maria","12312312312","email@email.com",null, "maria", null);
+//	System.out.println("\n--------cadastrar usuario---------------\n");
+//	UsuarioVo user1 = new UsuarioVo(0,"Clara","12312312313","email@email.com",LocalDate.of(2020,12,03), "clara", "clara");
 //	user1 = usuarioBo.cadastrarUsuarioBo(user1);
 //	System.out.println(user1.getIdUsuario() > 0 ? "Usuário cadastrado com sucesso!" : "Houve um problema no cadastro");
-//	
 //	System.out.println("\n-----------------------\n");
 //	
-	//consultar um usuario existente
-	UsuarioVo user3 = new UsuarioVo(1, "", "", "", null, "", "");
+//	System.out.println("\n--------cadastrar CPF nulo (validado pelo banco)---------------\n");
+//	UsuarioVo user1 = new UsuarioVo(0,"igor",null,"email@email.com",LocalDate.of(2019,02,02), "igor", "igor");
+//	user1 = usuarioBo.cadastrarUsuarioBo(user1);
+//	System.out.println(user1.getIdUsuario() > 0 ? "Usuário cadastrado com sucesso!" : "Houve um problema no cadastro");
+//	System.out.println("\n-----------------------\n");
+	
+	
+//	System.out.println("\n--------atualizar cadastro---------------\n");
+//	UsuarioVo user2 = new UsuarioVo(5,"","","",null, "", "");
+//	user2 = usuarioBo.consultarUsuarioBo(user2);
+//	user2.setCpf("12345678999");
+//	boolean resultado = usuarioBo.atualizarUsuarioBo(user2);
+//	System.out.println(resultado ? "Usuário atualizado com sucesso!" : "Houve um problema na atualização");
+//	System.out.println("\n-----------------------\n");
+	
+	System.out.println("\n--------excluir cadastro---------------\n");
+	UsuarioVo user4 = new UsuarioVo(6,"","","",null, "", "");
+	user4 = usuarioBo.consultarUsuarioBo(user4);
+	boolean resultado = usuarioBo.excluirUsuarioBo(user4);
+	System.out.println(resultado ? "Usuário excluído com sucesso!" : "Houve um problema na exclusão");
+	System.out.println("\n-----------------------\n");
+	
+	
+	System.out.println("\n--------consultar um usuario existente---------------\n");
+	UsuarioVo user3 = new UsuarioVo(5, "", "", "", null, "", "");
 	user3 = usuarioBo.consultarUsuarioBo(user3);
 	if(user3.getCpf() != null) {
 		System.out.println(user3);
 	}else {
-		System.out.println("Não Usuario não localizado");
+		System.out.println("Usuario não localizado");
 	}
+	System.out.println("\n-----------------------\n");
 	
-//	//consultar um usuario inexistente
-//		UsuarioVo user2 = new UsuarioVo(100,"","","",null, "", null);
-//		user2 = usuarioBo.consultarUsuarioBo(user2);
-//		if(user2.getCpf() != null) {
-//			System.out.println(user2);
-//		}else {
-//			System.out.println("Usuario não localizado na base de dados");
-//		}
-		
-//	//consultar todos usuarios
-//	ArrayList<UsuarioVo> lista = new ArrayList<UsuarioVo>();
-//	if(lista.isEmpty()) {
-//		System.out.println("tem nada");
-//	}else {
-//		System.out.println("olha ae");
-//		for(UsuarioVo u: lista) {
-//			System.out.println(u + "\n");
-//		}
-//	}
+	
+	System.out.println("\n----------consultar todos os usuários-------------\n");
+	ArrayList<UsuarioVo> lista = usuarioBo.consultarTodosUsuariosBo();
+	if(lista.isEmpty()) {
+		System.out.println("Não existem usuários cadastrados");
+	}else {
+		System.out.println("\n Lista de usuários: \n");
+		for(UsuarioVo u: lista) {
+			System.out.println(u + "\n");
+		}
+	}
 }
 
 }
