92d9b9f1d54bf654e34d262f6f1489b89ff49495
Thiago Camillo <thiago.sam.camillo@gmail.com>
2023-12-01 17:43:47 -0300

    refatorando

--- Projeto Web/senhor-financas/src/main/java/model/dao/DespesaDAO.java
+++ Projeto Web/senhor-financas/src/main/java/model/dao/DespesaDAO.java
@@ -129,10 +129,11 @@ public class DespesaDAO {
 		Connection conn = Banco.getConnection();
 		Statement stmt = Banco.getStatement(conn);
 		boolean retorno = false;
-		String query = "UPDATE despesa SET descricao = '" + despesaVO.getDescricao() + "', valor = '"
-				+ despesaVO.getValor() + "', datavencimento = '" + despesaVO.getDataVencimento()
-				+ " ', dataPagamento = '" + despesaVO.getDataPagamento() + "WHERE iddespesa = "
-				+ despesaVO.getIdDespesa();
+		String query = "UPDATE despesa SET descricao = " + despesaVO.getDescricao() 
+				+ "', valor = '" + despesaVO.getValor() 
+				+ "', datavencimento = '" + despesaVO.getDataVencimento()
+				+ "', dataPagamento = '" + despesaVO.getDataPagamento() 
+				+ "WHERE iddespesa = " + despesaVO.getIdDespesa();
 		try {
 			if (stmt.executeUpdate(query) == 1) {
 				retorno = true;
